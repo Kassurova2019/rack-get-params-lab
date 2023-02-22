2b4d06f07efd97db984e34edb7b73b84852c106f
Luba Kassurova <kassurova@icloud.com>
2023-02-22 16:58:51 -0500

    commit tree 7442493f70b33398486b01fbeb4f58750f41dc6f

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
+require "pry"
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -13,7 +14,24 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    else
+    elsif req.path.match(/cart/)
+      if @@cart.empty? 
+        resp.write "Your cart is empty"
+      else
+      @@cart.each do |cart|
+        resp.write "#{cart}\n"
+      end
+      end 
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      #binding.pry
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else resp.write "We don't have that item"
+      end
+
+    else 
       resp.write "Path Not Found"
     end
 
@@ -27,4 +45,6 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+
 end
